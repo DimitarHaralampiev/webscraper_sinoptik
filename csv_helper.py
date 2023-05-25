ba49734add8ba8169e701cb8f0859d3cdb5a15c1
DimitarHaralampiev <dimitur.haralampiev@gmail.com>
2023-05-25 12:36:05 +0300

    Divide SCVHelper class and SinoptikScraper to difference files

---
+++ csv_helper.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+class CSVHelper:
+    """
+    A helper class for writing data to CSV files.
+    """
+
+    def __init__(self, data, filename):
+        self.data = data
+        self.filename = filename
+
+    def write_csv(self):
+        """
+        Writes the data to a CSV file.
+        """
+        df = pd.DataFrame(self.data)
+        df.to_csv(self.filename, index=False)
--- scraper_sinoptik.py
+++ scraper_sinoptik.py
@@ -117,23 +117,6 @@ class SinoptikScraper:
         return weather_data_forecast
 
 
-class CSVHelper:
-    """
-    A helper class for writing data to CSV files.
-    """
-
-    def __init__(self, data, filename):
-        self.data = data
-        self.filename = filename
-
-    def write_csv(self):
-        """
-        Writes the data to a CSV file.
-        """
-        df = pd.DataFrame(self.data)
-        df.to_csv(self.filename, index=False)
-
-
 def current_weather(town: str):
     """
     Retrieves and saves the current weather data for a given town.
