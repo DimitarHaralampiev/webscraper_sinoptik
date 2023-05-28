c5f7a3535fc1919c5423119a5a51593c446efce4
DimitarHaralampiev <dimitur.haralampiev@gmail.com>
2023-05-28 19:44:58 +0300

    Remove class BuilderCSVFiles and change with pipeline

--- main.py
+++ main.py
@@ -1,12 +1,42 @@
-from building_csv_files import BuildingCSVFilesForWeather
+from csv_helper import CSVHelper
+from scraper_sinoptik import SinoptikScraper
+
+
+def retrieve_and_save_current_weather(town):
+    """
+    Retrieves and saves the current weather data for a given town.
+    """
+    try:
+        scrape = SinoptikScraper()
+        entry = scrape.scrape_current_weather(town)
+        if entry:
+            weather_data = CSVHelper(entry, 'weather_data.csv')
+            weather_data.write_csv()
+    except ValueError:
+        print('ERROR retrieving or saving current weather data')
+
+
+def retrieve_and_save_forecast_weather(town, period):
+    """
+    Retrieves and saves the weather forecast data for the next ten days for a given town.
+    """
+    try:
+        scrape = SinoptikScraper()
+        forecast_weather = scrape.scrape_weather_ten_days(town, period)
+        if forecast_weather:
+            weather_data = CSVHelper(forecast_weather, 'forecast_weather_data.csv')
+            weather_data.write_csv()
+    except ValueError:
+        print('ERROR retrieving or saving forecast weather data')
+
 
 town = input('Please enter Town: ')
 period = input('Please enter Period: ')
 
 
 if __name__ == '__main__':
-    weather_data = BuildingCSVFilesForWeather(town, period)
-    weather_data.retrieve_and_save_current_weather()
-    weather_data.retrieve_and_save_forecast_weather()
+
+    retrieve_and_save_current_weather(town)
+    retrieve_and_save_forecast_weather(town, period)
 
 
