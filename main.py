0a9d9cf85ec2395a78417b35e2529e61c9717c67
DimitarHaralampiev <dimitur.haralampiev@gmail.com>
2023-06-14 20:33:29 +0300

    feat: Add two methods to pipeline

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import csv
-
 from csv_helper import CSVHelper
 from scraper_sinoptik import SinoptikScraper
 from sql_helper import SQLiteDataStore
+
+
+scraper = SinoptikScraper()
+
+database_file = 'weather_data.db'
+helper = SQLiteDataStore(database_file)
 
 
 def retrieve_and_save_current_weather(town):
@@ -10,8 +14,7 @@ def retrieve_and_save_current_weather(town):
     Retrieves and saves the current weather data for a given town.
     """
     try:
-        scrape = SinoptikScraper()
-        entry = scrape.scrape_current_weather(town)
+        entry = scraper.scrape_current_weather(town)
         if entry:
             weather_data = CSVHelper(entry, 'weather_data.csv')
             weather_data.write_csv()
@@ -24,8 +27,7 @@ def retrieve_and_save_forecast_weather(town, period):
     Retrieves and saves the weather forecast data for the next ten days for a given town.
     """
     try:
-        scrape = SinoptikScraper()
-        forecast_weather = scrape.scrape_weather_ten_days(town, period)
+        forecast_weather = scraper.scrape_weather_ten_days(town, period)
         if forecast_weather:
             weather_data = CSVHelper(forecast_weather, 'forecast_weather_data.csv')
             weather_data.write_csv()
@@ -33,18 +35,49 @@ def retrieve_and_save_forecast_weather(town, period):
         print('ERROR retrieving or saving forecast weather data')
 
 
+def retrieve_and_save_to_database_current_weather(town):
+    """
+    Retrieve the current weather data for a given town and save it to the database.
+
+    Args:
+        town (str): The name of the town to retrieve weather data for.
+    """
+    try:
+        current_weather_data = scraper.scrape_current_weather(town)
+        for data in current_weather_data:
+            helper.write(data)
+    except ValueError as v:
+        print(f'Error retrieving or saving current weather data: {str(v)}')
+
+
+def retrieve_and_save_to_database_forecast_weather(town, period):
+    """
+    Retrieve the current weather data for a given town and save it to the database.
+
+    Args:
+        town (str): The name of the town to retrieve weather data for.
+    """
+    try:
+        forecast_weather_data = scraper.scrape_weather_ten_days(town, period)
+        for data in forecast_weather_data:
+            helper.write(data)
+    except ValueError as v:
+        print(f'Error retrieving or saving forecast weather data: {str(v)}')
+
+
 town = input('Please enter Town: ')
 period = input('Please enter Period: ')
 
 if __name__ == '__main__':
 
-    # retrieve_and_save_current_weather(town)
-    # retrieve_and_save_forecast_weather(town, period)
-
-    database_file = 'weather_data.db'
-    helper = SQLiteDataStore(database_file)
+    retrieve_and_save_current_weather(town)
+    retrieve_and_save_forecast_weather(town, period)
 
     helper.create_tables()
+
+    retrieve_and_save_to_database_current_weather(town)
+    retrieve_and_save_to_database_forecast_weather(town, period)
+
 
 
 
