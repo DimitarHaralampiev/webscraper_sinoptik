9583d3aca88daf9bbdc3960512b992142d583c01
DimitarHaralampiev <dimitur.haralampiev@gmail.com>
2023-06-25 19:57:09 +0300

    feat: Add config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.txt')
+
+database_name = config.get('CONFIG', 'database_name')
+base_url = config.get('CONFIG', 'base_url')
+town_name = config.get('CONFIG', 'town_name')
+period_weather = config.get('CONFIG', 'period_weather')
+weather_data_csv = config.get('CONFIG', 'weather_data_csv')
+forecast_weather_csv = config.get('CONFIG', 'forecast_weather_csv')
