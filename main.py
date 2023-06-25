0c3bc679c7dde557b8c96d84a3ccf41670baa205
DimitarHaralampiev <dimitur.haralampiev@gmail.com>
2023-06-25 16:16:04 +0300

    feat: Add argparse refactor: Refactoring methods

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import argparse
+
 from base_data_store import BaseDataStore
 from config import database_name, base_url, town_name, period_weather, weather_data_csv, forecast_weather_csv
+from csv_data_store import CSVDataStore
 from scraper_sinoptik import SinoptikScraper
 from sql_data_store import SQLiteDataStore
 
@@ -7,6 +10,19 @@ from sql_data_store import SQLiteDataStore
 scraper = SinoptikScraper(base_url)
 
 helper = SQLiteDataStore(database_name)
+
+parser = argparse.ArgumentParser(description='Weather Data Scraper')
+
+# Add the storage type argument
+parser.add_argument('--store', choices=['csv', 'db'], default='csv', help='Specify the storage type (csv or db)')
+# Add the database name argument
+parser.add_argument('--db_name', default=database_name, help='Specify the name or path of the database file')
+# Add the CSV filenames argument
+parser.add_argument('--weather-data-file', default=weather_data_csv, help='Name or path of the weather data file')
+parser.add_argument('--forecast-data-file', default=forecast_weather_csv, help='Name or path of the forecast data file')
+
+# Parse the arguments
+args = parser.parse_args()
 
 
 def retrieve_and_save_current_weather(town: str, data_store: BaseDataStore):
@@ -20,7 +36,10 @@ def retrieve_and_save_current_weather(town: str, data_store: BaseDataStore):
     try:
         entry = scraper.scrape_current_weather(town)
         if entry:
-            data_store.write(entry)
+            if args.store == 'csv':
+                data_store.write([entry])
+            elif args.store == 'db':
+                data_store.write(entry)
     except ValueError:
         print('ERROR retrieving or saving current weather data')
 
@@ -37,18 +56,19 @@ def retrieve_and_save_forecast_weather(town: str, period: str, data_store: BaseD
     try:
         forecast_weather = scraper.scrape_weather_ten_days(town, period)
         if forecast_weather:
-            for data in forecast_weather:
-                data_store.write(data)
+            if args.store == 'csv':
+                data_store.write(forecast_weather)
+            elif args.store == 'db':
+                for data in forecast_weather:
+                    data_store.write(data)
     except ValueError:
         print('ERROR retrieving or saving forecast weather data')
 
 
 if __name__ == '__main__':
 
-    retrieve_and_save_current_weather(town_name)
-    retrieve_and_save_forecast_weather(town_name, period_weather)
 
-    helper.create_tables()
+
 
 
 
