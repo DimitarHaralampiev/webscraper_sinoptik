cc84b58794ae92061fbde24e59493b2c9ea651dc
DimitarHaralampiev <dimitur.haralampiev@gmail.com>
2023-06-26 17:09:55 +0300

    feat: Add try except

--- main.py
+++ main.py
@@ -53,32 +53,35 @@ def retrieve_and_save_forecast_weather(town: str, period: str, save_type: str,  
 
 
 def main():
+    try:
+        parser = argparse.ArgumentParser(description='Weather Data Scraper')
+        # Add the storage type argument
+        parser.add_argument('--store', choices=['csv', 'db'], default='db', help='Specify the storage type (csv or db)')
+        # Add the database name argument
+        parser.add_argument('--db_name', default=database_name, help='Specify the name or path of the database file')
+        # Add the CSV filenames argument
+        parser.add_argument('--weather-data-file', default=weather_data_csv,
+                            help='Name or path of the weather data file')
+        parser.add_argument('--forecast-data-file', default=forecast_weather_csv,
+                            help='Name or path of the forecast data file')
+        # Parse the arguments
+        args = parser.parse_args()
 
-    parser = argparse.ArgumentParser(description='Weather Data Scraper')
-    # Add the storage type argument
-    parser.add_argument('--store', choices=['csv', 'db'], default='db', help='Specify the storage type (csv or db)')
-    # Add the database name argument
-    parser.add_argument('--db_name', default=database_name, help='Specify the name or path of the database file')
-    # Add the CSV filenames argument
-    parser.add_argument('--weather-data-file', default=weather_data_csv,
-                        help='Name or path of the weather data file')
-    parser.add_argument('--forecast-data-file', default=forecast_weather_csv,
-                        help='Name or path of the forecast data file')
-    # Parse the arguments
-    args = parser.parse_args()
+        if args.store == 'csv':
+            # Use CSVDataStore for storage
+            weather_data = CSVDataStore(args.weather_data_file)
+            forecast_data = CSVDataStore(args.forecast_data_file)
+            retrieve_and_save_current_weather(town_name, args.store, weather_data)
+            retrieve_and_save_forecast_weather(town_name, period_weather, args.store, forecast_data)
+        elif args.store == 'db':
+            # Use SQLiteDataStore for storage
+            data_store = SQLiteDataStore(args.db_name)
+            data_store.create_tables()
+            retrieve_and_save_current_weather(town_name, args.store, data_store)
+            retrieve_and_save_forecast_weather(town_name, period_weather, args.store, data_store)
 
-    if args.store == 'csv':
-        # Use CSVDataStore for storage
-        weather_data = CSVDataStore(args.weather_data_file)
-        forecast_data = CSVDataStore(args.forecast_data_file)
-        retrieve_and_save_current_weather(town_name, args.store, weather_data)
-        retrieve_and_save_forecast_weather(town_name, period_weather, args.store, forecast_data)
-    elif args.store == 'db':
-        # Use SQLiteDataStore for storage
-        data_store = SQLiteDataStore(args.db_name)
-        data_store.create_tables()
-        retrieve_and_save_current_weather(town_name, args.store, data_store)
-        retrieve_and_save_forecast_weather(town_name, period_weather, args.store, data_store)
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
 
 if __name__ == '__main__':
